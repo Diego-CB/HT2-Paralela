e5042f37adf77a13973614145834f5544133cefa
Diego-CB <cordova15441@gmail.com>
2023-09-26 19:21:01 -0600

    lab done

--- mpi_hello_3.c
+++ mpi_hello_3.c
@@ -1,5 +1,6 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
     MPI_Init(NULL, NULL); // Iniciar ambiente de MPI
@@ -18,12 +19,11 @@ int main(int argc, char** argv) {
     MPI_Get_processor_name(processor_name, &name_len);
 
     // Imprimir mensaje
-    printf(
-        "Hello world from processor %s, rank %d out of %d processors\n",
-        processor_name,
-        world_rank,
-        world_size
-    );
+    if (world_rank == 0) {
+        printf("Hi I'm process %d out of %d, and im receiving!\n", world_rank, world_size);
+    } else {
+        printf("Hi I'm process %d out of %d, and im sending!\n", world_rank, world_size);
+    }
 
     if (world_rank == 0) {
         for (int i = 1; i < world_size; i++) {
@@ -31,12 +31,20 @@ int main(int argc, char** argv) {
             // Recibir mensajes de los otros procesos
             MPI_Recv(message, 100, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             printf("%s\n", message);
+
+            char send_message[100];
+            snprintf(send_message, 100, "Sending response from process 0 to process %d", i);
+            MPI_Send(send_message, strlen(send_message) + 1, MPI_CHAR, i, 0, MPI_COMM_WORLD);
         }
     } else {
         // Los procesos 1, 2 y 3 envían mensajes al proceso 0
         char message[100];
-        snprintf(message, 100, "Greetings process 0, I'm process %d of %d", world_rank, world_size);
+        snprintf(message, 100, "Sending message from process %d of %d to process 0", world_rank, world_size);
         MPI_Send(message, strlen(message) + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+        
+        char recv_message[100];
+        MPI_Recv(recv_message, 100, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("%s\n", recv_message);
     }
 
     MPI_Finalize(); // Finalizar el ambiente
